93aceb5f37cc86c9ef1ed7665b63e484f0fa08f2
adarshprajapati012 <adarshprajapati012@gmail.com>
2023-11-22 03:44:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+Shery.imageEffect("#back", { style: 5, config: { "a": { "value": 2, "range": [0, 30] }, "b": { "value": -0.92, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 2.2034882866801215 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 0.96, "range": [0.1, 5] }, "durationIn": { "value": 0.32, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": false }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": false }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.76, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2], "_gsap": { "id": 3 } }, "discard_threshold": { "value": 0.68, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.49, "range": [0, 2] }, "noise_scale": { "value": 10.69, "range": [0, 100] } }, gooey: true })
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (elem) {
+    var h1s = elem.querySelectorAll("h1");
+    var index = 0;
+    document.querySelector("#main").addEventListener("click", function () {
+        gsap.to(h1s[index], {
+            top: "-=100%",
+            ease: Expo.easeInOut,
+            duration: 1,
+            onComplete: function () {
+                gsap.set(this._targets[0], { top: "100%" });
+            },
+        });
+
+        index === h1s.length - 1 ? (index = 0) : index++;
+
+        gsap.to(h1s[index], {
+            top: "-=100%",
+            ease: Expo.easeInOut,
+            duration: 1,
+        });
+    });
+})
